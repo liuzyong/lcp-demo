5455a1f120f5eac461bfffb8132b9c23b5041e8e
Rock <you@example.com>
2020-10-05 19:30:13 +0800

    增加sql语句

---
+++ base_service/conf/uniappadmin.sql
@@ -0,0 +1,194 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 39.105.197.166
+Source Server Version : 50621
+Source Host           : 39.105.197.166:3306
+Source Database       : offshorewindata
+
+Target Server Type    : MYSQL
+Target Server Version : 50621
+File Encoding         : 65001
+
+Date: 2020-10-05 19:23:00
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for attributes
+-- ----------------------------
+DROP TABLE IF EXISTS `attributes`;
+CREATE TABLE `attributes` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `source_id` bigint(20) unsigned NOT NULL COMMENT '目标id',
+  `type` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '自定义字段',
+  `language` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'zh-CN' COMMENT '语言',
+  `remark` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '字段名备注',
+  `value` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '字段',
+  `format` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '数据类型',
+  `v1` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v2` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v3` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v4` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v5` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `v6` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `type_name_source_id` (`type`,`name`,`source_id`) USING BTREE COMMENT 'source_id_key_language'
+) ENGINE=InnoDB AUTO_INCREMENT=100351297529577473 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for authorizations
+-- ----------------------------
+DROP TABLE IF EXISTS `authorizations`;
+CREATE TABLE `authorizations` (
+  `id` bigint(11) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `resource` varchar(255) DEFAULT NULL COMMENT '资源',
+  `entity` varchar(255) DEFAULT NULL COMMENT '实体',
+  `operation` varchar(255) DEFAULT NULL COMMENT '操作权限',
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for categories
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` bigint(20) unsigned NOT NULL COMMENT 'id',
+  `parent_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '父ID',
+  `level` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '层级',
+  `path` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `type` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `categories_parent_id_index` (`parent_id`),
+  KEY `categories_type` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for configurations
+-- ----------------------------
+DROP TABLE IF EXISTS `configurations`;
+CREATE TABLE `configurations` (
+  `id` bigint(20) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for files
+-- ----------------------------
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `id` bigint(20) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` bigint(20) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `parent_id` bigint(11) DEFAULT NULL COMMENT '父级订单',
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for pay
+-- ----------------------------
+DROP TABLE IF EXISTS `pay`;
+CREATE TABLE `pay` (
+  `id` bigint(20) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for prices
+-- ----------------------------
+DROP TABLE IF EXISTS `prices`;
+CREATE TABLE `prices` (
+  `id` bigint(20) unsigned NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `source_id` bigint(20) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remark` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `value` decimal(20,8) DEFAULT NULL,
+  `language` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `format` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for products
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=98521305891995649 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for relations
+-- ----------------------------
+DROP TABLE IF EXISTS `relations`;
+CREATE TABLE `relations` (
+  `id` bigint(20) DEFAULT NULL,
+  `type` varchar(64) DEFAULT NULL,
+  `source_id` bigint(20) DEFAULT NULL COMMENT '如产品id ',
+  `target_id` bigint(20) DEFAULT NULL COMMENT '如分类id',
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  UNIQUE KEY `type_source_id_target_id` (`type`,`source_id`,`target_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for specifications
+-- ----------------------------
+DROP TABLE IF EXISTS `specifications`;
+CREATE TABLE `specifications` (
+  `id` bigint(20) unsigned NOT NULL,
+  `product_id` bigint(20) unsigned NOT NULL COMMENT '商品id',
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `username` varchar(64) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `phone` varchar(64) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `created_time` timestamp NULL DEFAULT NULL,
+  `updated_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
