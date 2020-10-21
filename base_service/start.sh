68916e92410d1d8417868b6e394b05d9ceb8f951
Rock <you@example.com>
2020-10-21 17:18:30 +0800

    提交

--- base_service/start.sh
+++ base_service/start.sh
@@ -8,16 +8,14 @@ if [ ! "$mysql" ]; then
 /etc/init.d/mysql start 
 sleep 10
 fi
-sleep 5
-done
 
-
-while true; do
 server=`ps aux | grep /mnt/uniappadmin/base_service/main  | grep -v grep`
 echo $server
 if [ ! "$server" ]; then
 nohup  /mnt/uniappadmin/base_service/main -c 1 &
 sleep 10
 fi
+
 sleep 5
 done
+
